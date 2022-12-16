21ad62c46d4623881cd1804474c74ba552e73ef6
byunboy2 <76236947+byunboy2@users.noreply.github.com>
2022-12-16 11:45:24 -0800

    Update bubble.js

--- dsa-sorting-1/bubble.js
+++ dsa-sorting-1/bubble.js
@@ -1,3 +1,13 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  //[4, 20, 12, 10, 7, 9]
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if(arr[j]>arr[j+1]){
+        [arr[j],arr[j+1]]= [arr[j+1],arr[j]]
+      }
+    }
+  }
+  return arr
+}
 
 module.exports = bubbleSort;
